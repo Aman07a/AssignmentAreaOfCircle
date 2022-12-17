aec04af77d9504a52ec122c48931829e04b032d2
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-17 15:13:40 +0100

    Submitted Assignment: AreaOfCircle

--- AssignmentAreaOfCircle/Program.cs
+++ AssignmentAreaOfCircle/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Write a C# program to calculate "area of circle" based on the given "radius" value.
+The radius value can be assigned to a variable; need not to take the value from the keyboard input.
+
+Formula: PI * radius * radius
+
+PI is a constant value, approximately equal to 3.14159, which is equal to the ratio of the circumference of any circle to its diameter. 
+*/
 
 namespace AssignmentAreaOfCircle
 {
@@ -6,7 +13,17 @@ namespace AssignmentAreaOfCircle
 	{
 		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			// Inputs
+			double PI = 3.14159;
+			System.Console.WriteLine("What is the radius of the circle?");
+			int radius = int.Parse(System.Console.ReadLine());
+
+			// Process
+			double area = PI * radius * radius;
+
+			// Output
+			System.Console.WriteLine("\nThe area of the circle is " + area);
+			System.Console.Read();
 		}
 	}
 }
